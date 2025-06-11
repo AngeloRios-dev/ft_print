2916f9a63bfa272d3c57bed9c859e23ac72ff379
Angelo Rios <angelorios@live.com>
2025-06-11 13:42:14 +0200

    ref: remove unneeded comments

--- ft_handlers.c
+++ ft_handlers.c
@@ -6,38 +6,11 @@
 /*   By: angrios <angrios@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 21:25:23 by angrios           #+#    #+#             */
-/*   Updated: 2025/06/09 17:33:20 by angrios          ###   ########.fr       */
+/*   Updated: 2025/06/11 13:32:22 by angrios          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-// int	ft_int_handler(int num)
-// {
-// 	int		count;
-// 	long	temp;
-
-// 	count = 0;
-// 	if (num == 0)
-// 	{
-// 		ft_putchar_fd('0', 1);
-// 		return (1);
-// 	}
-// 	if (num < 0)
-// 	{
-// 		count++;
-// 		temp = -(long)num;
-// 	}
-// 	else
-// 		temp = num;
-// 	while (temp > 0)
-// 	{
-// 		temp /= 10;
-// 		count++;
-// 	}
-// 	ft_putnbr_fd(num, 1);
-// 	return (count);
-// }
 
 int	ft_int_handler(int num)
 {
@@ -57,27 +30,6 @@ int	ft_int_handler(int num)
 	ft_putchar_fd((nb % 10) + '0', 1);
 	return (++count);	
 }
-
-// int	ft_unsigned_handler(unsigned int num)
-// {
-// 	int				count;
-// 	unsigned int	temp;
-
-// 	if (num == 0)
-// 	{
-// 		ft_putchar_fd('0', 1);
-// 		return (1);
-// 	}
-// 	count = 0;
-// 	temp = num;
-// 	while (temp > 0)
-// 	{
-// 		temp /= 10;
-// 		count++;
-// 	}
-// 	ft_print_unsigned(num);
-// 	return (count);
-// }
 
 int	ft_unsigned_handler(unsigned int num)
 {
